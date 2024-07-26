70986719810ea20cb72b2ee95c2827e77fb1342d
Louis Mandel <lmandel@us.ibm.com>
2024-07-26 11:55:05 -0400

    Streaming of the results during benchmarks execution (#273)

--- pdl/benchmark.py
+++ pdl/benchmark.py
@@ -2,7 +2,7 @@ import argparse
 import json
 import sys
 from io import StringIO
-from multiprocessing import Pool
+from multiprocessing import Manager, Pool
 
 import yaml
 
@@ -26,71 +26,24 @@ class BaseProcessor:
         return 0.0
 
     def process_all(self):
-        with open("log.out", "w", encoding="utf-8") as log:
-            with open(self.dataset, "r", encoding="utf-8") as json_file:
-                json_list = list(json_file)
-            with open(self.pdl_file, "r", encoding="utf-8") as pdl:
-                obj = yaml.safe_load(pdl)
-                data = Program.model_validate(obj)
-                matches = 0  # pylint: disable=invalid-name
-                exceptions = 0  # pylint: disable=invalid-name
-                with Pool(processes=self.nb_processes) as pool:
-                    jobs = [
-                        pool.apply_async(self.process_sample, (data, index, json_str))
-                        for index, json_str in enumerate(json_list)
-                    ]
-                    answers = [res.get() for res in jobs]
-                for (
-                    index,
-                    question,
-                    truth,
-                    answer,
-                    solution,
-                    document,
-                    exception,
-                ) in answers:
-                    print("\nQuestion: " + question)
-                    print(answer)
-                    if exception is not None:
-                        print("EXCEPTION at: " + str(index))
-                        print(exception)
-                        exceptions += 1
-                        write_log(
-                            log,
-                            index,
-                            question,
-                            truth,
-                            answer,
-                            solution,
-                            document,
-                            exception,
-                        )
-                    if answer == truth or document.endswith(str(truth)):
-                        print("MATCH!")
-                        matches += 1
-                    else:
-                        print("NO MATCH!")
-                        print("Truth: " + str(truth))
-                        print("Question: " + question)
-                        write_log(
-                            log,
-                            index,
-                            question,
-                            truth,
-                            answer,
-                            solution,
-                            document,
-                            None,
-                        )
-                    print(
-                        "Percentage passing: "
-                        + str(matches / (index + 1))
-                        + " ("
-                        + str(index + 1)
-                        + " completed)"
-                    )
+        with open(self.dataset, "r", encoding="utf-8") as json_file:
+            json_list = list(json_file)
+        with open(self.pdl_file, "r", encoding="utf-8") as pdl:
+            obj = yaml.safe_load(pdl)
+            data = Program.model_validate(obj)
+        manager = Manager()
+        q = manager.Queue()
+        with Pool(processes=self.nb_processes + 1) as pool:
+            pool.apply_async(self.accumulator, (q,))
+            jobs = [
+                pool.apply_async(self.process_sample, (q, data, index, json_str))
+                for index, json_str in enumerate(json_list)
+            ]
+            for res in jobs:
+                res.get()
+            q.put(None)
 
-    def process_sample(self, data, index, json_str):
+    def process_sample(self, q, data, index, json_str):
         qna = json.loads(json_str)
         question = self.get_question(qna)
         truth, solution = self.get_truth_answer(qna)
@@ -105,15 +58,67 @@ class BaseProcessor:
             answer = self.extract_answer(document)
         except Exception as e:
             exception = e
-        return (
-            index,
-            question,
-            truth,
-            answer,
-            solution,
-            document,
-            exception,
-        )
+        q.put((index, question, truth, answer, solution, document, exception))
+
+    def accumulator(self, q):
+        with open("log.out", "w", encoding="utf-8") as log:
+            completed = 0
+            matches = 0  # pylint: disable=invalid-name
+            exceptions = 0  # pylint: disable=invalid-name
+            while True:
+                m = q.get()
+                completed += 1
+                if m is None:
+                    break
+                (
+                    index,
+                    question,
+                    truth,
+                    answer,
+                    solution,
+                    document,
+                    exception,
+                ) = m
+                print("\nQuestion: " + question)
+                print(answer)
+                if exception is not None:
+                    print("EXCEPTION at: " + str(index))
+                    print(exception)
+                    exceptions += 1
+                    write_log(
+                        log,
+                        index,
+                        question,
+                        truth,
+                        answer,
+                        solution,
+                        document,
+                        exception,
+                    )
+                if answer == truth or document.endswith(str(truth)):
+                    print("MATCH!")
+                    matches += 1
+                else:
+                    print("NO MATCH!")
+                    print("Truth: " + str(truth))
+                    print("Question: " + question)
+                    write_log(
+                        log,
+                        index,
+                        question,
+                        truth,
+                        answer,
+                        solution,
+                        document,
+                        None,
+                    )
+                print(
+                    "Percentage passing: "
+                    + str(matches / completed)
+                    + " ("
+                    + str(completed)
+                    + " completed)"
+                )
 
 
 def write_log(  # pylint: disable=too-many-arguments
@@ -261,7 +266,7 @@ if __name__ == "__main__":
         help="benchmark",
     )
     parser.add_argument(
-        "--processes", "-n", const=10, type=int, help="number of processes"
+        "--processes", "-n", nargs="?", default=10, type=int, help="number of processes"
     )
 
     args = parser.parse_args()
