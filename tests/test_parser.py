cd79abadc3be40cfba5eb7e2dda057db8317f8a4
Mandana Vaziri <mvaziri@us.ibm.com>
2024-09-14 13:59:55 -0400

    Mock results for LLM calls in test suite (#57)
    * mock responses for llm calls
    
    Signed-off-by: Mandana Vaziri <mvaziri@us.ibm.com>
    
    * removing secrets from build file
    
    Signed-off-by: Mandana Vaziri <mvaziri@us.ibm.com>
    
    ---------
    
    Signed-off-by: Mandana Vaziri <mvaziri@us.ibm.com>

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -21,7 +21,11 @@ model_parser = {
         ]
     },
     "parser": "json",
-    "parameters": {"stop_sequences": ["}"], "include_stop_sequence": True},
+    "parameters": {
+        "stop_sequences": ["}"],
+        "include_stop_sequence": True,
+        "mock_response": '{"bob": 20, "carol": 30}',
+    },
 }
 
 
