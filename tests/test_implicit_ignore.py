65ad94db8d691750689b517d8d23c2a15984cf9e
Louis Mandel <lmandel@us.ibm.com>
2024-10-04 16:57:57 -0400

    Add more tests

--- tests/test_implicit_ignore.py
+++ tests/test_implicit_ignore.py
@@ -9,20 +9,6 @@ def do_test(capsys, test):
     assert result == test["result"]
     assert captured.err == test["warnings"]
 
-def test_no_warning(capsys):
-    test = { "prog": """
-text:
-- Hello
-- How are you?
-- Bye
-""",
-    "result": "Bye",
-    "warnings": """Warning: the result of block `Hello` is not used.
-         You might want to use a `text` block or explicitly ignore the result with `contribute: [context]`.
-Warning: the result of block `How are you?` is not used.
-"""
-    }
-    do_test(capsys, test)
 
 
 def test_strings(capsys):
@@ -36,5 +22,30 @@ def test_strings(capsys):
          You might want to use a `text` block or explicitly ignore the result with `contribute: [context]`.
 Warning: the result of block `How are you?` is not used.
 """
+    }
+    do_test(capsys, test)
+
+def test_no_warning1(capsys):
+    test = { "prog": """
+text:
+- Hello
+- How are you?
+- Bye
+""",
+    "result": "HelloHow are you?Bye",
+    "warnings": ""
+    }
+    do_test(capsys, test)
+
+def test_no_warning1(capsys):
+    test = { "prog": """
+- def: x
+  text: Hello
+- text: How are you?
+  contribute: []
+- Bye
+""",
+    "result": "Bye",
+    "warnings": ""
     }
     do_test(capsys, test)
