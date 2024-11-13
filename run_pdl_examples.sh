e28b5eee02e19072e94e1e0ecdce67816807b472
Louis Mandel <lmandel@us.ibm.com>
2024-11-12 21:16:22 -0500

    Remove debug step

---
+++ run_pdl_examples.sh
@@ -0,0 +1,240 @@
+# #!/usr/bin/env bash
+# cd "$(dirname "$0")/.." || exit
+
+# echo "Running examples/callback/repair_prompt.pdl"
+# python -m src.pdl.pdl "examples/callback/repair_prompt.pdl"
+
+# echo "Running examples/chatbot/chatbot.pdl"
+# python -m src.pdl.pdl "examples/chatbot/chatbot.pdl"
+
+# echo "Running examples/cldk/cldk-assistant.pdl"
+# python -m src.pdl.pdl "examples/cldk/cldk-assistant.pdl"
+
+# echo "Running examples/code/code-eval.pdl"
+# python -m src.pdl.pdl "examples/code/code-eval.pdl"
+
+# echo "Running examples/code/code-json.pdl"
+# python -m src.pdl.pdl "examples/code/code-json.pdl"
+
+# echo "Running examples/code/code.pdl"
+# python -m src.pdl.pdl "examples/code/code.pdl"
+
+# echo "Running examples/demo/1-gen-data.pdl"
+# python -m src.pdl.pdl "examples/demo/1-gen-data.pdl"
+
+# echo "Running examples/demo/2-teacher.pdl"
+# python -m src.pdl.pdl "examples/demo/2-teacher.pdl"
+
+# echo "Running examples/demo/3-weather.pdl"
+# python -m src.pdl.pdl "examples/demo/3-weather.pdl"
+
+# echo "Running examples/demo/4-translator.pdl"
+# python -m src.pdl.pdl "examples/demo/4-translator.pdl"
+
+# echo "Running examples/fibonacci/fib.pdl"
+# python -m src.pdl.pdl "examples/fibonacci/fib.pdl"
+
+# echo "Running examples/granite/granite_defs.pdl"
+# python -m src.pdl.pdl "examples/granite/granite_defs.pdl"
+
+# echo "Running examples/granite/multi_round_chat.pdl"
+# python -m src.pdl.pdl "examples/granite/multi_round_chat.pdl"
+
+# echo "Running examples/granite/single_round_chat.pdl"
+# python -m src.pdl.pdl "examples/granite/single_round_chat.pdl"
+
+# echo "Running examples/hello/hello-code.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-code.pdl"
+
+# echo "Running examples/hello/hello-data.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-data.pdl"
+
+# echo "Running examples/hello/hello-def-use.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-def-use.pdl"
+
+# echo "Running examples/hello/hello-defs.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-defs.pdl"
+
+# echo "Running examples/hello/hello-for-loop.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-for-loop.pdl"
+
+# echo "Running examples/hello/hello-function-alias.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-function-alias.pdl"
+
+# echo "Running examples/hello/hello-function.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-function.pdl"
+
+# echo "Running examples/hello/hello-if.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-if.pdl"
+
+# echo "Running examples/hello/hello-iteration.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-iteration.pdl"
+
+# echo "Running examples/hello/hello-model-chaining.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-model-chaining.pdl"
+
+# echo "Running examples/hello/hello-model-input.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-model-input.pdl"
+
+# echo "Running examples/hello/hello-parser-json.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-parser-json.pdl"
+
+# echo "Running examples/hello/hello-parser-regex.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-parser-regex.pdl"
+
+# echo "Running examples/hello/hello-roles-array.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-roles-array.pdl"
+
+# echo "Running examples/hello/hello-type-code.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-type-code.pdl"
+
+# echo "Running examples/hello/hello-type-list.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-type-list.pdl"
+
+# echo "Running examples/hello/hello-type.pdl"
+# python -m src.pdl.pdl "examples/hello/hello-type.pdl"
+
+# echo "Running examples/hello/hello.pdl"
+# python -m src.pdl.pdl "examples/hello/hello.pdl"
+
+# echo "Running examples/input/input_test.pdl"
+# python -m src.pdl.pdl "examples/input/input_test.pdl"
+
+# echo "Running examples/input/input_test1.pdl"
+# python -m src.pdl.pdl "examples/input/input_test1.pdl"
+
+# echo "Running examples/input/input_test2.pdl"
+# python -m src.pdl.pdl "examples/input/input_test2.pdl"
+
+# echo "Running examples/rag/rag.pdl"
+# python -m src.pdl.pdl "examples/rag/rag.pdl"
+
+# echo "Running examples/react/demo.pdl"
+# python -m src.pdl.pdl "examples/react/demo.pdl"
+
+# echo "Running examples/react/react_call.pdl"
+# python -m src.pdl.pdl "examples/react/react_call.pdl"
+
+# echo "Running examples/react/react_fun.pdl"
+# python -m src.pdl.pdl "examples/react/react_fun.pdl"
+
+# echo "Running examples/react/wikipedia.pdl"
+# python -m src.pdl.pdl "examples/react/wikipedia.pdl"
+
+# echo "Running examples/sdk/hello.pdl"
+# python -m src.pdl.pdl "examples/sdk/hello.pdl"
+
+# echo "Running examples/talk/1-hello.pdl"
+# python -m src.pdl.pdl "examples/talk/1-hello.pdl"
+
+# echo "Running examples/talk/10-sdg.pdl"
+# python -m src.pdl.pdl "examples/talk/10-sdg.pdl"
+
+# echo "Running examples/talk/11-repeat.pdl"
+# python -m src.pdl.pdl "examples/talk/11-repeat.pdl"
+
+# echo "Running examples/talk/2-model-chaining.pdl"
+# python -m src.pdl.pdl "examples/talk/2-model-chaining.pdl"
+
+# echo "Running examples/talk/3-def-use.pdl"
+# python -m src.pdl.pdl "examples/talk/3-def-use.pdl"
+
+# echo "Running examples/talk/4-function.pdl"
+# python -m src.pdl.pdl "examples/talk/4-function.pdl"
+
+# echo "Running examples/talk/5-code-eval.pdl"
+# python -m src.pdl.pdl "examples/talk/5-code-eval.pdl"
+
+# echo "Running examples/talk/6-code-json.pdl"
+# python -m src.pdl.pdl "examples/talk/6-code-json.pdl"
+
+# echo "Running examples/talk/7-chatbot-roles.pdl"
+# python -m src.pdl.pdl "examples/talk/7-chatbot-roles.pdl"
+
+# echo "Running examples/talk/8-tools.pdl"
+# python -m src.pdl.pdl "examples/talk/8-tools.pdl"
+
+# echo "Running examples/talk/9-react.pdl"
+# python -m src.pdl.pdl "examples/talk/9-react.pdl"
+
+# echo "Running examples/teacher/teacher.pdl"
+# python -m src.pdl.pdl "examples/teacher/teacher.pdl"
+
+# echo "Running examples/tools/calc.pdl"
+# python -m src.pdl.pdl "examples/tools/calc.pdl"
+
+# echo "Running examples/tutorial/calling_apis.pdl"
+# python -m src.pdl.pdl "examples/tutorial/calling_apis.pdl"
+
+# echo "Running examples/tutorial/calling_code.pdl"
+# python -m src.pdl.pdl "examples/tutorial/calling_code.pdl"
+
+# echo "Running examples/tutorial/calling_llm.pdl"
+# python -m src.pdl.pdl "examples/tutorial/calling_llm.pdl"
+
+# echo "Running examples/tutorial/calling_llm_with_input.pdl"
+# python -m src.pdl.pdl "examples/tutorial/calling_llm_with_input.pdl"
+
+# echo "Running examples/tutorial/calling_llm_with_input_messages.pdl"
+# python -m src.pdl.pdl "examples/tutorial/calling_llm_with_input_messages.pdl"
+
+# echo "Running examples/tutorial/conditionals_loops.pdl"
+# python -m src.pdl.pdl "examples/tutorial/conditionals_loops.pdl"
+
+# echo "Running examples/tutorial/data_block.pdl"
+# python -m src.pdl.pdl "examples/tutorial/data_block.pdl"
+
+# echo "Running examples/tutorial/data_block_raw.pdl"
+# python -m src.pdl.pdl "examples/tutorial/data_block_raw.pdl"
+
+# echo "Running examples/tutorial/for.pdl"
+# python -m src.pdl.pdl "examples/tutorial/for.pdl"
+
+# echo "Running examples/tutorial/for_array.pdl"
+# python -m src.pdl.pdl "examples/tutorial/for_array.pdl"
+
+# echo "Running examples/tutorial/for_multiplie_lists.pdl"
+# python -m src.pdl.pdl "examples/tutorial/for_multiplie_lists.pdl"
+
+# echo "Running examples/tutorial/for_with.pdl"
+# python -m src.pdl.pdl "examples/tutorial/for_with.pdl"
+
+# echo "Running examples/tutorial/function_definition.pdl"
+# python -m src.pdl.pdl "examples/tutorial/function_definition.pdl"
+
+# echo "Running examples/tutorial/gen-data.pdl"
+# python -m src.pdl.pdl "examples/tutorial/gen-data.pdl"
+
+echo "Running examples/tutorial/grouping_definitions.pdl"
+python -m src.pdl.pdl "examples/tutorial/grouping_definitions.pdl"
+
+echo "Running examples/tutorial/include.pdl"
+python -m src.pdl.pdl "examples/tutorial/include.pdl"
+
+echo "Running examples/tutorial/input_file.pdl"
+python -m src.pdl.pdl "examples/tutorial/input_file.pdl"
+
+echo "Running examples/tutorial/input_file_json.pdl"
+python -m src.pdl.pdl "examples/tutorial/input_file_json.pdl"
+
+echo "Running examples/tutorial/input_stdin.pdl"
+python -m src.pdl.pdl "examples/tutorial/input_stdin.pdl"
+
+echo "Running examples/tutorial/input_stdin_multiline.pdl"
+python -m src.pdl.pdl "examples/tutorial/input_stdin_multiline.pdl"
+
+echo "Running examples/tutorial/model_chaining.pdl"
+python -m src.pdl.pdl "examples/tutorial/model_chaining.pdl"
+
+echo "Running examples/tutorial/muting_block_output.pdl"
+python -m src.pdl.pdl "examples/tutorial/muting_block_output.pdl"
+
+echo "Running examples/tutorial/simple_program.pdl"
+python -m src.pdl.pdl "examples/tutorial/simple_program.pdl"
+
+echo "Running examples/tutorial/variable_def_use.pdl"
+python -m src.pdl.pdl "examples/tutorial/variable_def_use.pdl"
+
+echo "Running examples/weather/weather.pdl"
+python -m src.pdl.pdl "examples/weather/weather.pdl"
+echo "All examples have been run."
