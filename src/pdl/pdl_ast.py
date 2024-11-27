2ee203e0ea92f7c68608e9b941db8b36c3fef8c7
Mandana Vaziri <mvaziri@us.ibm.com>
2024-11-27 14:30:46 -0500

    cleanup
    Signed-off-by: Mandana Vaziri <mvaziri@us.ibm.com>

--- src/pdl/pdl_ast.py
+++ src/pdl/pdl_ast.py
@@ -636,9 +636,9 @@ def set_structured_decoding_parameters(
     
     if spec is not None and parameters["response_format"] is None and "guided_decoding_backend" not in parameters :
         schema = pdltype_to_jsonschema(spec, True)
-        #parameters["guided_decoding_backend"] = "lm-format-enforcer"
-        #parameters["guided_json"] = schema
-        parameters["response_format"] = { "type": "json_schema", "json_schema": schema , "strict": True }
+        parameters["guided_decoding_backend"] = "lm-format-enforcer"
+        parameters["guided_json"] = schema
+        #parameters["response_format"] = { "type": "json_schema", "json_schema": schema , "strict": True }
     return parameters
 
 def set_default_granite_model_parameters(
