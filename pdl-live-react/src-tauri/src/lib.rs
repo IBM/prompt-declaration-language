e10b315b55a9630b2ecd96b3a5f2d9a4a465c114
Nick Mitchell <nickm@us.ibm.com>
2025-01-30 14:34:48 -0500

    feat: allow opening a trace file directly from the command line
    in dev mode:
    ```shell
    ./src-tauri/target/debug/PDL view ./src/demos/demo5.json
    ```
    
    For some reason AppImage builds are failing in github actions. So we won't yet enable production app build tests.
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src-tauri/src/lib.rs
+++ pdl-live-react/src-tauri/src/lib.rs
@@ -1,15 +1,109 @@
+use serde_json::Value;
+use std::fs;
+use std::path::Path;
+use urlencoding::encode;
+
+use tauri::ipc::Response;
+use tauri_plugin_cli::CliExt;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn read_trace(trace_file: &str) -> Response {
+    let data = fs::read(trace_file).unwrap();
+    Response::new(data)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle().plugin(tauri_plugin_cli::init())?;
+
+            // let tmp =  app.handle().path().app_local_data_dir().join("mytraces");
+
+            //let tmp_dir = TempDir::new("pdl")?;
+            //println!("TMP {:?}", tmp_dir);
+            //let scope = app.fs_scope();
+            //let _ = scope.allow_directory(tmp_dir.path(), false); // TODO error handling
+
+            // Default to GUI if the app was opened with no CLI args.
+            if std::env::args_os().count() <= 1 {
+                gui(app.handle().clone(), "".to_owned())?;
+            }
+            match app.cli().matches() {
+                // `matches` here is a Struct with { args, subcommand }.
+                // `args` is `HashMap<String, ArgData>` where `ArgData` is a struct with { value, occurrences }.
+                // `subcommand` is `Option<Box<SubcommandMatches>>` where `SubcommandMatches` is a struct with { name, matches }.
+                Ok(matches) => {
+                    match matches.subcommand {
+                        Some(subcommand_matches) => {
+                            match subcommand_matches.name.as_str() {
+                                "view" => {
+                                    match subcommand_matches.matches.args.get("trace") {
+                                        Some(trace) => {
+                                            match &trace.value {
+                                                Value::String(trace_file) => {
+                                                    // app.handle().plugin(tauri_plugin_fs::init())?;
+
+                                                    //let src = Path::new(trace_file);
+                                                    //let name = src.file_name().unwrap();
+                                                    //let tmp =  app.path().app_local_data_dir().join("mytraces").join(name);
+                                                    //fs::copy(src, &tmp)?;
+
+                                                    // allowed access to the trace directory
+                                                    // let src = Path::new(&trace_file);
+                                                    //let canon = fs::canonicalize(src)?;
+                                                    //let abs = canon.as_path();
+                                                    //println!("!!!!!!!!!!!! {:?}", abs);
+                                                    //let src = &abs.display().to_string();
+
+                                                    let encoded = encode(trace_file);
+                                                    gui(
+                                                        app.handle().clone(),
+                                                        Path::new("/local")
+                                                            .join(encoded.as_ref())
+                                                            .display()
+                                                            .to_string(),
+                                                    )?
+                                                }
+                                                _ => {
+                                                    println!("Usage: view <tracefile.json>");
+                                                    std::process::exit(1)
+                                                }
+                                            }
+                                        }
+                                        _ => {
+                                            println!("Usage: view <tracefile.json>");
+                                            std::process::exit(1)
+                                        }
+                                    }
+                                }
+                                _ => {}
+                            }
+                        }
+                        None => {}
+                    }
+                    //println!(" {:?}", matches);
+                    //gui(app.handle().clone(), "".to_owned())?;
+                }
+                Err(_) => {}
+            }
+            Ok(())
+        })
         .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![read_trace])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("error while running PDL");
+}
+
+fn gui(app: tauri::AppHandle, path: String) -> Result<(), tauri::Error> {
+    #[cfg(all(not(debug_assertions), windows))]
+    remove_windows_console();
+    tauri::WebviewWindowBuilder::new(&app, "label", tauri::WebviewUrl::App(path.into()))
+        .title("Prompt Declaration Language")
+        .inner_size(1400.0, 1050.0)
+        .build()?;
+    Ok(())
 }
