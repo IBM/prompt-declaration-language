4b914db064e3b51e071af1633ffee8cb53338727
Nick Mitchell <nickm@us.ibm.com>
2025-02-03 11:42:32 -0500

    fix: in react ui, add icons to welcome tiles
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src/page/welcome/Tile.tsx
+++ pdl-live-react/src/page/welcome/Tile.tsx
@@ -6,20 +6,31 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
+  Divider,
   Flex,
 } from "@patternfly/react-core"
 
 type Props = PropsWithChildren<{
   title: ReactNode
   body: ReactNode
+  icon?: ReactNode
 }>
 
-export default function Tile({ title, body, children }: Props) {
+const gapSm = { default: "gapSm" as const }
+const center = { default: "alignItemsCenter" as const }
+
+export default function Tile({ title, body, children, icon }: Props) {
   return (
     <Card isLarge>
       <CardHeader>
-        <CardTitle>{title}</CardTitle>
+        <CardTitle>
+          <Flex gap={gapSm} alignItems={center}>
+            {icon}
+            {title}
+          </Flex>
+        </CardTitle>
       </CardHeader>
+      <Divider />
       <CardBody>{body}</CardBody>
       <CardFooter>
         <Flex>{children}</Flex>
