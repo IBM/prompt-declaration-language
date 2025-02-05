d160bb4619ded4d48280981a2caa9bc329690379
Nick Mitchell <nickm@us.ibm.com>
2025-02-05 17:53:26 -0500

    fix: in react ui, MyTracesPage emits react key warning
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src/page/MyTracesPage.tsx
+++ pdl-live-react/src/page/MyTracesPage.tsx
@@ -7,40 +7,45 @@ import Result from "../view/transcript/Result"
 
 import { hasResult } from "../helpers"
 import { getMyTraces } from "./MyTraces"
+import { nonNullable } from "../helpers"
 
 import MyIcon from "@patternfly/react-icons/dist/esm/icons/user-icon"
 
 function myTiles() {
-  return getMyTraces().map(({ title, filename, value }) => {
-    try {
-      // TODO useMemo()
-      const data = JSON.parse(value)
-      return (
-        <Tile
-          key={filename}
-          title={title}
-          icon={<MyIcon />}
-          body={
-            hasResult(data) ? (
-              <Panel isScrollable>
-                <PanelMain maxHeight="200px">
-                  <Result result={data.result} />
-                </PanelMain>
-              </Panel>
-            ) : (
-              <>View {title}</>
-            )
-          }
-        >
-          <Button isInline variant="link">
-            <Link to={"/my/" + encodeURIComponent(title)}>Show this Trace</Link>
-          </Button>
-        </Tile>
-      )
-    } catch (_err) {
-      return <></>
-    }
-  })
+  return getMyTraces()
+    .map(({ title, filename, value }) => {
+      try {
+        // TODO useMemo()
+        const data = JSON.parse(value)
+        return (
+          <Tile
+            key={filename}
+            title={title}
+            icon={<MyIcon />}
+            body={
+              hasResult(data) ? (
+                <Panel isScrollable>
+                  <PanelMain maxHeight="200px">
+                    <Result result={data.result} />
+                  </PanelMain>
+                </Panel>
+              ) : (
+                <>View {title}</>
+              )
+            }
+          >
+            <Button isInline variant="link">
+              <Link to={"/my/" + encodeURIComponent(title)}>
+                Show this Trace
+              </Link>
+            </Button>
+          </Tile>
+        )
+      } catch (_err) {
+        return null
+      }
+    })
+    .filter(nonNullable)
 }
 
 export default function MyTraces() {
