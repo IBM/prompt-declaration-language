19ca496d1aa7d8e97e95ede2c8a1e68754dc1b6b
Nick Mitchell <nickm@us.ibm.com>
2025-02-05 15:45:22 -0500

    fix: add error boundary to react ui
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src/routes/PdlRoutes.tsx
+++ pdl-live-react/src/routes/PdlRoutes.tsx
@@ -6,37 +6,39 @@ import Local from "../page/Local"
 import MyTrace from "../page/MyTrace"
 import Welcome from "../page/welcome/Welcome"
 import Uploader from "../page/Uploader"
-import PageNotFound from "../page/PageNotFound"
+import ErrorBoundary from "../page/ErrorBoundary"
 
 import demos from "../demos/demos"
 import { getMyTraces } from "../page/MyTraces"
 
 export default function PdlRoutes() {
   return (
-    <Routes>
-      <Route index element={<Welcome />} />
-      <Route path="welcome" element={<Welcome />} />
-      <Route path="local/:traceFile" element={<Local />} />
-      <Route path="about" element={<About />} />
-      <Route path="upload" element={<Uploader />} />
+    <ErrorBoundary>
+      <Routes>
+        <Route index element={<Welcome />} />
+        <Route path="welcome" element={<Welcome />} />
+        <Route path="local/:traceFile" element={<Local />} />
+        <Route path="about" element={<About />} />
+        <Route path="upload" element={<Uploader />} />
 
-      {demos.map((demo) => (
-        <Route
-          key={demo.name}
-          path={`/demos/${demo.name}`}
-          element={<Demo name={demo.name} value={demo.trace} />}
-        />
-      ))}
+        {demos.map((demo, idx) => (
+          <Route
+            key={demo.name + "." + idx}
+            path={`/demos/${demo.name}`}
+            element={<Demo name={demo.name} value={demo.trace} />}
+          />
+        ))}
 
-      {getMyTraces().map(({ title, filename, value }) => (
-        <Route
-          key={filename}
-          path={`/my/${title}`}
-          element={<MyTrace name={title} value={value} />}
-        />
-      ))}
+        {getMyTraces().map(({ title, filename, value }, idx) => (
+          <Route
+            key={filename + "." + idx}
+            path={`/my/${title}`}
+            element={<MyTrace name={title} value={value} />}
+          />
+        ))}
 
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+        <Route path="*" element={<ErrorBoundary />} />
+      </Routes>
+    </ErrorBoundary>
   )
 }
