a9d4fa146ff441215a54f2822fbf7cc4b7715fcd
Nick Mitchell <nickm@us.ibm.com>
2025-02-12 13:42:03 -0500

    refactor: in react ui, extract `findNode` from `DrawerContent`
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

---
+++ pdl-live-react/src/view/detail/find.ts
@@ -0,0 +1,28 @@
+import { childrenOf } from "../timeline/model"
+
+import {
+  isNonScalarPdlBlock,
+  nonNullable,
+  type NonScalarPdlBlock as Block,
+} from "../../helpers"
+
+/**
+ * Traverse the tree under `block` looking for a sub-block with then
+ * given `id`.
+ */
+export default function find(
+  block: import("../../pdl_ast").PdlBlock,
+  id: string,
+): null | Block {
+  if (!isNonScalarPdlBlock(block)) {
+    return null
+  } else if (block.id === id) {
+    return block
+  } else {
+    return (
+      childrenOf(block)
+        .map((child) => find(child, id))
+        .filter(nonNullable)[0] || null
+    )
+  }
+}
