30b8435156e1904aab9768cd766f5d37f061c68a
Nick Mitchell <nickm@us.ibm.com>
2025-02-18 15:16:16 -0500

    feat: in react ui re-enable Replay/Run button and functionality
    I had to disable this temporarily as the prior implementation was blocking macos notarization. With this update, instead of inlining an entire venv as a tauri resource, we inline only the requirements.txt.
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src-tauri/src/cli/run.rs
+++ pdl-live-react/src-tauri/src/cli/run.rs
@@ -1,18 +1,91 @@
-use std::fs::read;
+use file_diff::diff;
+use std::fs::{copy, create_dir_all, read};
 use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
 use std::process::{Command, Stdio};
 use tempfile::NamedTempFile;
+
+use tauri::path::BaseDirectory;
+use tauri::Manager;
+
+#[cfg(desktop)]
+fn pip_install_if_needed(app_handle: tauri::AppHandle) -> Result<String, tauri::Error> {
+    let cache_path = app_handle.path().cache_dir()?.join("pdl");
+
+    create_dir_all(&cache_path)?;
+    let venv_path = cache_path.join("interpreter-python");
+    let activate_path = venv_path
+        .join("bin/activate")
+        .into_os_string()
+        .into_string()
+        .unwrap();
+    let cached_requirements_path = venv_path
+        .join("requirements.txt")
+        .into_os_string()
+        .into_string()
+        .unwrap();
+    /* println!(
+        "RUN PATHS activate={:?} cached_reqs={:?}",
+        activate_path, cached_requirements_path
+    ); */
+
+    if !venv_path.exists() {
+        println!("Creating virtual environment...");
+        let venv_path_string = venv_path.into_os_string().into_string().unwrap();
+        let output = Command::new("python3.12")
+            .args(["-mvenv", venv_path_string.as_str()])
+            .output()
+            .expect("Failed to execute venv creation");
+        println!("{:?}", output);
+    }
+
+    let requirements_path = app_handle
+        .path()
+        .resolve("interpreter/requirements.txt", BaseDirectory::Resource)?
+        .into_os_string()
+        .into_string()
+        .unwrap();
+
+    if !diff(
+        requirements_path.as_str(),
+        cached_requirements_path.as_str(),
+    ) {
+        println!("Running pip install...");
+        let mut child = Command::new("sh")
+            .args([
+                "-c",
+                format!(
+                    "source {activate} && pip install -r {requirements}",
+                    activate = activate_path,
+                    requirements = requirements_path
+                )
+                .as_str(),
+            ])
+            .stdout(Stdio::piped())
+            .spawn()
+            .unwrap();
+
+        let stdout = child.stdout.take().unwrap();
+
+        // Stream output.
+        let lines = BufReader::new(stdout).lines();
+        for line in lines {
+            println!("{}", line.unwrap());
+        }
+
+        copy(requirements_path, cached_requirements_path)?;
+    }
+
+    Ok(activate_path)
+}
 
 #[cfg(desktop)]
 pub fn run_pdl_program(
     source_file_path: String,
-    interpreter_path: PathBuf,
+    app_handle: tauri::AppHandle,
     trace: bool,
 ) -> Result<Option<Vec<u8>>, tauri::Error> {
     println!("Running {:?}", source_file_path);
-    //let interp = interpreter_path.display().to_string()
-    let activate = interpreter_path.join("bin/activate").display().to_string();
+    let activate = pip_install_if_needed(app_handle)?;
 
     let (trace_file, trace_arg) = if trace {
         let f = NamedTempFile::new()?;
