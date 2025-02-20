a866b07ab1c5fc13c378a4507a0c0774965bf528
Nick Mitchell <nickm@us.ibm.com>
2025-02-20 10:06:38 -0500

    fix: react ui shows object Object for text->data blocks
    This also fixes rendering of JSON data in a few places (that were showing the json data as a string)
    
    Adds demo8.json.
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src/demos/demos.ts
+++ pdl-live-react/src/demos/demos.ts
@@ -5,17 +5,25 @@ import demo4 from "./demo4.json"
 import demo5 from "./demo5.json"
 import demo6 from "./demo6.json"
 import demo7 from "./demo7.json"
+import demo8 from "./demo8.json"
 
 type Demo = {
   name: string
   trace: string
 }
 
-const demos: Demo[] = [demo1, demo2, demo3, demo4, demo5, demo6, demo7].map(
-  (demo) => ({
-    name: demo.description,
-    trace: JSON.stringify(demo),
-  }),
-)
+const demos: Demo[] = [
+  demo1,
+  demo2,
+  demo3,
+  demo4,
+  demo5,
+  demo6,
+  demo7,
+  demo8,
+].map((demo) => ({
+  name: demo.description,
+  trace: JSON.stringify(demo),
+}))
 
 export default demos
