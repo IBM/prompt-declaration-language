bcf35289675ce0f1e27d60b4a1b7ca0d7b21ff22
Nick Mitchell <nickm@us.ibm.com>
2025-02-20 10:06:38 -0500

    fix: react ui shows object Object for text->data blocks
    This also fixes rendering of JSON data in a few places (that were showing the json data as a string)
    
    Adds demo8.json.
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src/view/detail/kind/Model.tsx
+++ pdl-live-react/src/view/detail/kind/Model.tsx
@@ -2,7 +2,7 @@ import Group from "../Group"
 import Result from "../../Result"
 
 export default function ModelItems({
-  block: { model, input, result },
+  block: { model, input, result, parser },
 }: {
   block: import("../../../pdl_ast").LitellmModelBlock
 }) {
@@ -10,7 +10,16 @@ export default function ModelItems({
     <>
       {typeof model === "string" && <Group term="Model" description={model} />}
       {typeof input === "string" && <Group term="Input" description={input} />}
-      <Result result={result} />
+      <Result
+        result={result}
+        lang={
+          parser === "jsonl" || parser === "json"
+            ? "json"
+            : parser === "yaml"
+              ? "yaml"
+              : "plaintext"
+        }
+      />
     </>
   )
 }
--- pdl-live-react/src/view/masonry/model.ts
+++ pdl-live-react/src/view/masonry/model.ts
@@ -1,3 +1,4 @@
+import { stringify } from "yaml"
 import { computeModel as computeBaseModel } from "../timeline/model"
 
 import {
@@ -5,8 +6,6 @@ import {
   hasParser,
   hasScalarResult,
   hasTimingInformation,
-  isNonScalarPdlBlock,
-  isTextBlock,
   type NonScalarPdlBlock,
 } from "../../helpers"
 
@@ -17,53 +16,28 @@ export default function computeModel(block: import("../../pdl_ast").PdlBlock) {
   const masonry: Tile[] = base
     .filter(({ block }) => block.kind !== "if")
     .flatMap(({ id, block, children }) => {
-      if (isTextBlock(block)) {
-        if (!Array.isArray(block.text)) {
-          if (isNonScalarPdlBlock(block.text)) {
-            const content = String(block.text).trim()
-            if (content.length > 0) {
-              return [{ id, content }]
-            }
-          }
-        } else {
-          return withDefs(
-            block,
-            block.text.flatMap((child, idx) => {
-              if (
-                !isNonScalarPdlBlock(child) &&
-                (typeof child !== "string" || child.trim().length > 0)
-              ) {
-                return [
-                  {
-                    id: `${id}.${idx}`,
-                    content: String(child).trim(),
-                  },
-                ]
-              }
-              return []
-            }),
-          )
-        }
-      } else if (
-        children.length === 0 &&
-        hasScalarResult(block) &&
-        hasTimingInformation(block)
-      ) {
+      if (children.length === 0 && hasTimingInformation(block)) {
         return withDefs(block, [
           {
             id,
             def: block.def,
             kind: block.kind,
             message: hasMessage(block) ? block.message : undefined,
-            content: String(block.result),
+            content:
+              typeof block.result === "object"
+                ? stringify(block.result)
+                : String(block.result),
             start_nanos: block.start_nanos,
             end_nanos: block.end_nanos,
             timezone: block.timezone,
-            lang: hasParser(block)
-              ? block.parser === "jsonl"
-                ? "json"
-                : block.parser
-              : undefined,
+            lang:
+              typeof block.result === "object"
+                ? "yaml"
+                : hasParser(block)
+                  ? block.parser === "jsonl"
+                    ? "json"
+                    : block.parser
+                  : undefined,
             crumb: true,
           },
         ])
