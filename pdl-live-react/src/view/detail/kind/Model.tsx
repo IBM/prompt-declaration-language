d3d7d4040d0a9ca8f3185c351cf254f8da1754ff
Nick Mitchell <nickm@us.ibm.com>
2025-02-25 10:07:35 -0500

    feat: update masonry ui to display structured model response meta
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src/view/detail/kind/Model.tsx
+++ pdl-live-react/src/view/detail/kind/Model.tsx
@@ -2,65 +2,27 @@ import { stringify } from "yaml"
 
 import Group from "../Group"
 import Result from "../../Result"
-import { capitalizeAndUnSnakeCase } from "../../../helpers"
-
-function tryJson(s: unknown) {
-  if (typeof s === "string") {
-    try {
-      return JSON.parse(s)
-    } catch (_err) {
-      // intentional fall-through
-    }
-  }
-  return s
-}
+import {
+  capitalizeAndUnSnakeCase,
+  extractStructuredModelResponse,
+} from "../../../helpers"
 
 export default function ModelItems({
-  block: { platform, model, input, result, parser },
+  block,
 }: {
   block: import("../../../pdl_ast").LitellmModelBlock
 }) {
-  // All of this JSON stuff is to handle structured responses from the
-  // model
-  const json = tryJson(result)
-  const resultForDisplay = Array.isArray(json)
-    ? json.map(({ sentence }) => sentence).join("\n")
-    : result
+  const { platform, model, input } = block
+  const { resultForDisplay, lang, meta } = extractStructuredModelResponse(block)
 
-  const lang = Array.isArray(json)
-    ? undefined
-    : parser === "jsonl" || parser === "json"
-      ? "json"
-      : parser === "yaml"
-        ? "yaml"
-        : "plaintext"
-
-  // Ugh, some of this logic may be specific to Granite LLM
-  const meta = Array.isArray(json)
-    ? json.flatMap(({ meta }, idx) =>
-        Object.entries(meta)
-          .map(([k, v]) => {
-            if (
-              k === "citation" &&
-              v &&
-              typeof v === "object" &&
-              "snippet" in v
-            ) {
-              return [k, v.snippet]
-            } else {
-              return [k, v]
-            }
-          })
-          .map(([k, v]) => (
-            <Result
-              key={k + "." + idx}
-              term={capitalizeAndUnSnakeCase(String(k))}
-              result={typeof v === "object" ? stringify(v) : v}
-              lang={typeof v === "object" ? "yaml" : undefined}
-            />
-          )),
-      )
-    : undefined
+  const metaForDisplay = meta?.map(([k, v], idx) => (
+    <Result
+      key={k + "." + idx}
+      term={capitalizeAndUnSnakeCase(String(k))}
+      result={typeof v === "object" ? stringify(v) : v}
+      lang={typeof v === "object" ? "yaml" : undefined}
+    />
+  ))
 
   return (
     <>
@@ -70,7 +32,7 @@ export default function ModelItems({
       {typeof model === "string" && <Group term="Model" description={model} />}
       {typeof input === "string" && <Group term="Input" description={input} />}
       <Result result={resultForDisplay} lang={lang} />
-      {meta}
+      {metaForDisplay}
     </>
   )
 }
