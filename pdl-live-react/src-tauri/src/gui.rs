2c7c59d3f1cc6f65a09ce8a9d423c3339a4a0bd1
Nick Mitchell <nickm@us.ibm.com>
2025-03-01 12:54:25 -0500

    fix: try 5 re-enabling windows ui build automation
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src-tauri/src/gui.rs
+++ pdl-live-react/src-tauri/src/gui.rs
@@ -1,8 +1,6 @@
 use tauri::WebviewWindowBuilder;
 
 pub fn setup(app: tauri::AppHandle, path: String) -> Result<(), tauri::Error> {
-    #[cfg(all(not(debug_assertions), windows))]
-    remove_windows_console();
     WebviewWindowBuilder::new(&app, "main", tauri::WebviewUrl::App(path.into()))
         .title("Prompt Declaration Language")
         .zoom_hotkeys_enabled(true)
