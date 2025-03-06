e5b9d0bc2844a263cd27465f70123e851473dce9
Nick Mitchell <nickm@us.ibm.com>
2025-03-06 13:17:50 -0500

    feat: add Pagination to UI
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src/view/masonry/Masonry.tsx
+++ pdl-live-react/src/view/masonry/Masonry.tsx
@@ -3,12 +3,21 @@ import MasonryTile from "./MasonryTile"
 import "./Masonry.css"
 
 type Props = import("react").PropsWithChildren<{
+  page: number
+  perPage: number
   run: import("./MasonryCombo").Runner
   sml: import("./Toolbar").SML
   model: import("./Tile").default[]
 }>
 
-export default function Masonry({ run, sml, model, children }: Props) {
+export default function Masonry({
+  run,
+  sml,
+  model,
+  children,
+  page,
+  perPage,
+}: Props) {
   return (
     <div className="pdl-masonry-view" data-padding={sml}>
       {(!children ? [] : Array.isArray(children) ? children : [children])
@@ -23,11 +32,11 @@ export default function Masonry({ run, sml, model, children }: Props) {
             {child}
           </div>
         ))}
-      {model.map((props, idx) => (
+      {model.slice((page - 1) * perPage, page * perPage).map((props, idx) => (
         <MasonryTile
           key={props.id + "." + idx}
           {...props}
-          idx={idx + 1}
+          idx={(page - 1) * perPage + idx + 1}
           sml={sml}
           run={run}
         />
--- pdl-live-react/src/view/masonry/Toolbar.tsx
+++ pdl-live-react/src/view/masonry/Toolbar.tsx
@@ -1,6 +1,7 @@
 import { Toolbar, ToolbarGroup, ToolbarContent } from "@patternfly/react-core"
 
 import DarkModeToggle from "../../page/DarkModeToggle"
+import ToolbarPaginator from "./ToolbarPaginator"
 import ToolbarSMLToggle from "./ToolbarSMLToggle"
 import ToolbarReplayButton from "./ToolbarReplayButton"
 import ToolbarShowSourceButton from "./ToolbarShowSourceButton"
@@ -11,7 +12,7 @@ const alignEnd = { default: "alignEnd" as const }
 
 export type SML = "s" | "m" | "l" | "xl"
 
-export type Props = {
+export type Props = import("./ToolbarPaginator").Props & {
   run: import("./MasonryCombo").Runner
   isRunning: boolean
   block: import("../../pdl_ast").PdlBlock
@@ -26,6 +27,12 @@ export default function MasonryToolbar({
   isRunning,
   sml,
   setSML,
+
+  itemCount,
+  page,
+  perPage,
+  setPage,
+  setPerPage,
 }: Props) {
   return (
     <Toolbar className="pdl-masonry-toolbar">
@@ -34,6 +41,14 @@ export default function MasonryToolbar({
           <ToolbarReplayButton run={run} isRunning={isRunning} />
         </ToolbarGroup>
         <ToolbarGroup align={alignEnd} variant="action-group">
+          <ToolbarPaginator
+            itemCount={itemCount}
+            perPage={perPage}
+            page={page}
+            setPage={setPage}
+            setPerPage={setPerPage}
+          />
+
           {isNonScalarPdlBlock(block) && (
             <ToolbarShowSourceButton root={block.pdl__id ?? ""} />
           )}
