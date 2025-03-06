e314bcb868134b7183dfa23adc0f1cb5d8b37b3a
Nick Mitchell <nickm@us.ibm.com>
2025-03-06 10:24:34 -0500

    fix: RunMenu react callback lacks dependencies
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src/view/masonry/RunMenu.tsx
+++ pdl-live-react/src/view/masonry/RunMenu.tsx
@@ -68,7 +68,7 @@ export default function RunMenu({ block, run }: Props) {
         splitButtonItems={splitButtonItems}
       />
     ),
-    [],
+    [isMenuOpen, onMenuToggle, splitButtonItems],
   )
 
   return (
