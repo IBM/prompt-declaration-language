060d871e582e236f9f0670637d7a67246cc78e5c
Mandana Vaziri <mvaziri@us.ibm.com>
2025-03-07 10:45:15 -0500

    Fix to allow type checking of functions passsed as parameters to other functions
    Signed-off-by: Mandana Vaziri <mvaziri@us.ibm.com>

--- src/pdl/pdl_schema_validator.py
+++ src/pdl/pdl_schema_validator.py
@@ -4,6 +4,7 @@ from typing import Any, Optional
 from .pdl_location_utils import get_loc_string
 from .pdl_schema_error_analyzer import analyze_errors
 from .pdl_schema_utils import get_json_schema, pdltype_to_jsonschema
+from .pdl_ast import FunctionBlock
 
 
 def type_check_args(
@@ -25,6 +26,9 @@ def type_check_args(
     if "pdl_context" in args_copy:
         # params_copy["pdl_context"] = [{"role": "str?", "content": "str"}]
         params_copy["pdl_context"] = ["obj"]
+    for k,v in args_copy.items():
+        if isinstance(v, FunctionBlock):
+            args_copy[k] = v.model_dump()
     schema = get_json_schema(params_copy, False)
     if schema is None:
         return ["Error obtaining a valid schema from function parameters definition"]
