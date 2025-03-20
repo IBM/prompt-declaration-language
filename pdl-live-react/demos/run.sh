2b614b71121d142e20ff0fa86d0cf0f5351eea85
Nick Mitchell <nickm@us.ibm.com>
2025-03-20 13:43:11 -0400

    fix: ui demos/run.sh erroneously disabled generating demo9.json
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/demos/run.sh
+++ pdl-live-react/demos/run.sh
@@ -18,5 +18,4 @@ pdl --trace "$T"/demo5.json "$TOP"/examples/talk/6-code-json.pdl
 pdl --trace "$T"/demo6.json "$UI"/demos/error.pdl || true
 pdl --trace "$T"/demo7.json "$TOP"/examples/talk/4-function.pdl
 # TODO demo8...
-exit
 pdl --stream none --trace "$T"/demo9.json "$TOP"/examples/gsm8k/gsm8.pdl
