37d3c9e9dee88f760e9bf9007fb161b7faaab17c
Nick Mitchell <nickm@us.ibm.com>
2025-03-26 11:41:12 -0400

    chore: clean up messy rust code in cli::setup
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src-tauri/src/cli/setup.rs
+++ pdl-live-react/src-tauri/src/cli/setup.rs
@@ -31,68 +31,58 @@ pub fn cli(app: &mut tauri::App) -> Result<(), Box<dyn ::std::error::Error>> {
             let Some(compile_subcommand_matches) = subcommand_matches.matches.subcommand else {
                 return Err(Box::from("Missing compile subcommand"));
             };
+            let args = compile_subcommand_matches.matches.args;
 
             match compile_subcommand_matches.name.as_str() {
                 "beeai" => {
-                    // TODO this probably fails if the source is a number??
-                    let Some(ArgData {
-                        value: Value::String(source_file_path),
-                        ..
-                    }) = compile_subcommand_matches.matches.args.get("source")
-                    else {
-                        return Err(Box::from("Invalid source file"));
-                    };
-                    let Some(ArgData {
-                        value: Value::String(output_file_path),
-                        ..
-                    }) = compile_subcommand_matches.matches.args.get("output")
-                    else {
-                        return Err(Box::from("Invalid output argument"));
-                    };
-                    let Some(ArgData {
-                        value: Value::Bool(debug),
-                        ..
-                    }) = compile_subcommand_matches.matches.args.get("debug")
-                    else {
-                        return Err(Box::from("Invalid debug argument"));
-                    };
-                    return compile::beeai::compile(source_file_path, output_file_path, debug);
+                    match (args.get("source"), args.get("output"), args.get("debug")) {
+                        (
+                            // TODO this probably fails if the source is a number??
+                            Some(ArgData {
+                                value: Value::String(source_file_path),
+                                ..
+                            }),
+                            Some(ArgData {
+                                value: Value::String(output_file_path),
+                                ..
+                            }),
+                            Some(ArgData {
+                                value: Value::Bool(debug),
+                                ..
+                            }),
+                        ) => compile::beeai::compile(source_file_path, output_file_path, debug),
+                        _ => Err(Box::from("Invalid compile subcommand")),
+                    }
                 }
-                _ => {}
+                _ => Err(Box::from("Unsupported compile command")),
             }
         }
-        "run" => {
-            let Some(source) = subcommand_matches.matches.args.get("source") else {
-                return Err(Box::from("Missing source file"));
-            };
-            let Value::String(source_file_path) = &source.value else {
-                return Err(Box::from("Invalid source file argument"));
-            };
-            return run::run_pdl_program(
+        "run" => match subcommand_matches.matches.args.get("source") {
+            Some(ArgData {
+                value: Value::String(source_file_path),
+                ..
+            }) => run::run_pdl_program(
                 source_file_path.clone(),
                 app.handle().clone(),
                 subcommand_matches.matches.args.get("trace"),
                 subcommand_matches.matches.args.get("data"),
                 subcommand_matches.matches.args.get("stream"),
-            );
-        }
-        "view" => {
-            let Some(trace) = subcommand_matches.matches.args.get("trace") else {
-                return Err(Box::from("Missing trace file"));
-            };
-            let Value::String(trace_file) = &trace.value else {
-                return Err(Box::from("Invalid trace file argument"));
-            };
-            gui_setup(
+            ),
+            _ => Err(Box::from("Invalid source file argument")),
+        },
+        "view" => match subcommand_matches.matches.args.get("trace") {
+            Some(ArgData {
+                value: Value::String(trace_file),
+                ..
+            }) => gui_setup(
                 app.handle().clone(),
                 Path::new("/local")
                     .join(encode(trace_file).as_ref())
                     .display()
                     .to_string(),
-            )?
-        }
-        _ => {}
+            ),
+            _ => Err(Box::from("Invalid trace file argument")),
+        },
+        _ => Err(Box::from("Unsupported command")),
     }
-
-    Ok(())
 }
--- pdl-live-react/src-tauri/src/gui.rs
+++ pdl-live-react/src-tauri/src/gui.rs
@@ -1,6 +1,6 @@
 use tauri::WebviewWindowBuilder;
 
-pub fn setup(app: tauri::AppHandle, path: String) -> Result<(), tauri::Error> {
+pub fn setup(app: tauri::AppHandle, path: String) -> Result<(), Box<dyn ::std::error::Error>> {
     WebviewWindowBuilder::new(&app, "main", tauri::WebviewUrl::App(path.into()))
         .title("Prompt Declaration Language")
         .zoom_hotkeys_enabled(true)
