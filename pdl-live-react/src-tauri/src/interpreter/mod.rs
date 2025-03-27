2ec8da1f92d30a3eaa56e77b32eabdc2954780c5
Nick Mitchell <nickm@us.ibm.com>
2025-03-27 13:22:23 -0400

    chore: yet more rust code cleanups
    - move the run module under interpreter::run
    - improved handling of optional paths to gui open window
    - rename gui::setup to gui::new_window
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- pdl-live-react/src-tauri/src/interpreter/mod.rs
+++ pdl-live-react/src-tauri/src/interpreter/mod.rs
@@ -3,4 +3,5 @@ pub mod extract;
 pub mod pip;
 pub mod pull;
 pub mod requirements;
+pub mod run;
 pub mod shasum;
--- pdl-live-react/src-tauri/src/lib.rs
+++ pdl-live-react/src-tauri/src/lib.rs
@@ -13,9 +13,9 @@ pub fn run() {
         .setup(|app| {
             // Default to GUI if the app was opened with no CLI args.
             if args_os().count() <= 1 {
-                gui::setup(app.handle().clone(), "".to_owned())
+                gui::new_window(app.handle().clone(), None)
             } else {
-                match cli::setup::cli(app) {
+                match cli::setup(app) {
                     Ok(true) => ::std::process::exit(0), // success with CLI
                     Ok(false) => Ok(()), // instead, open GUI (fallthrough to the logic below)
                     Err(s) => {
