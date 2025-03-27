0c16321587efc453d33505b8358c86a03e15e1c0
Louis Mandel <lmandel@us.ibm.com>
2025-03-27 09:23:32 -0400

    Add tests

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -17,15 +17,11 @@ array:
             "role": "system",
             "content": "You are a helpful software engineer. You write clear, concise, well-commented code.",
             "defsite": "array.0.message",
-            "name": None,
-            "tool_call_id": None,
         },
         {
             "role": "user",
             "content": "Write a Python function that implement merge sort.",
             "defsite": "array.1.message",
-            "name": None,
-            "tool_call_id": None,
         },
     ]
     assert context == [
@@ -38,5 +34,36 @@ array:
             "role": "user",
             "content": "Write a Python function that implement merge sort.",
             "defsite": "array.1.message",
+        },
+    ]
+
+
+def test_messages2():
+    prog_str = """
+description: Messages block
+array:
+  - role: tool
+    content: 42
+    name: f
+    tool_call_id: id
+"""
+    result = exec_str(prog_str, output="all")
+    context = result["scope"]["pdl_context"]
+    assert result["result"] == [
+        {
+            "role": "tool",
+            "content": 42,
+            "name": "f",
+            "tool_call_id": "id",
+            "defsite": "array.0.message",
+        },
+    ]
+    assert context == [
+        {
+            "role": "tool",
+            "content": 42,
+            "name": "f",
+            "tool_call_id": "id",
+            "defsite": "array.0.message",
         },
     ]
