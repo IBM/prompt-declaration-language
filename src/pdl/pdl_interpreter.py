2cbb420e81f941ed32dbbaa81faba7780b183b30
Louis Mandel <lmandel@us.ibm.com>
2025-04-04 14:22:08 -0400

    Add tests
    Signed-off-by: Louis Mandel <lmandel@us.ibm.com>

--- src/pdl/pdl_interpreter.py
+++ src/pdl/pdl_interpreter.py
@@ -577,21 +577,18 @@ def process_block_body(
                 scope,
                 loc,
             )
-            name, block = process_expr_of(
-                block, "name", scope, loc  # pyright: ignore
-            )  # pyright: ignore
-            tool_call_id, block = process_expr_of(
-                block, "tool_call_id", scope, loc  # pyright: ignore
-            )  # pyright: ignore
-            result = PdlDict(
-                {
-                    "role": state.role,
-                    "content": content,
-                    "name": name,
-                    "tool_call_id": tool_call_id,
-                    "defsite": block.pdl__id,
-                }
-            )
+            d = {
+                "role": state.role,
+                "content": content,
+                "defsite": block.pdl__id,
+            }
+            if block.name is not None:
+                name, block = process_expr_of(block, "name", scope, loc)
+                d["name"] = name
+            if block.tool_call_id is not None:
+                tool_call_id, block = process_expr_of(block, "tool_call_id", scope, loc)
+                d["tool_call_id"] = tool_call_id
+            result = PdlDict(d)
             background = PdlList([result])
         case IfBlock():
             b, if_trace = process_condition_of(block, "condition", scope, loc, "if")
