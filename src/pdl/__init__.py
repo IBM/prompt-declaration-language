433f053dfb18c0d0cb4d4d88c717a8e89d0a108f
Louis Mandel <lmandel@us.ibm.com>
2025-05-02 12:05:49 -0400

    Revert changes to pdl/__init__.py
    Signed-off-by: Louis Mandel <lmandel@us.ibm.com>

--- src/pdl/__init__.py
+++ src/pdl/__init__.py
@@ -1,31 +1,6 @@
 from importlib.metadata import PackageNotFoundError, version
 
-from ._version import __version__ as hardcoded_version
-
-
-def _get_distribution_version(distribution_name: str) -> str:
-    """
-    This function attempts to retrieve the version of PDL package using
-    importlib.metadata.
-
-    When the package is not installed, importlib will raise a PackageNotFoundError.
-    In this case, we fallback to the hardcoded version.
-
-    When the package is installed, but the distribution name does not match,
-    importlib will return the version of the package that is installed.
-    """
-    try:
-        return version(distribution_name)
-    except PackageNotFoundError:
-        # This is a fallback for when the package is not recognized by importlib.metadata.
-        # This can happen when the package is not installed.
-        return (
-            hardcoded_version
-            if distribution_name == "pdl"
-            else _get_distribution_version(
-                "pdl"  # This is a fallback to maintain the previous behavior.
-            )
-        )
-
-
-__version__ = _get_distribution_version("prompt-declaration-language")
+try:
+    __version__ = version("pdl")
+except PackageNotFoundError:
+    pass
