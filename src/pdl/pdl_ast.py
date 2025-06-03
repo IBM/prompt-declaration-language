0b3dbd95e8bc6e132f9c77eeb9c41203e885cd33
Mandana Vaziri <mvaziri@us.ibm.com>
2025-06-03 13:48:37 -0400

    dependent/independent context implementation (#945)
    * dependent/independent context implementation
    
    Signed-off-by: Mandana Vaziri <mvaziri@us.ibm.com>

--- src/pdl/pdl_ast.py
+++ src/pdl/pdl_ast.py
@@ -26,6 +26,7 @@ from pydantic import (
 from pydantic.json_schema import SkipJsonSchema
 from typing_extensions import TypeAliasType
 
+from .pdl_context import PDLContext
 from .pdl_lazy import PdlDict, PdlLazy
 
 
@@ -42,7 +43,7 @@ ModelInput: TypeAlias = Sequence[Mapping[str, Any]]
 
 
 LazyMessage: TypeAlias = PdlLazy[dict[str, Any]]
-LazyMessages: TypeAlias = PdlLazy[list[dict[str, Any]]]
+LazyMessages: TypeAlias = PDLContext
 
 
 class BlockKind(StrEnum):
