fe9b71c9efa489b102a96fd901fc28ebbaa8a9ae
Mandana Vaziri <mvaziri@us.ibm.com>
2025-08-11 13:29:09 -0400

    pdl generative
    Signed-off-by: Mandana Vaziri <mvaziri@us.ibm.com>

--- src/pdl/pdl.py
+++ src/pdl/pdl.py
@@ -159,6 +159,11 @@ def exec_file(
     result = exec_program(program, config, scope, loc, output)
     return result
 
+def pdl(func):
+    def pdl_wrapper(scope):
+        result = exec_str(prog=func.__doc__, scope=scope)
+        return result
+    return pdl_wrapper
 
 def main():
     parser = argparse.ArgumentParser("")
