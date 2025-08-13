d557eee177c71f89d410d97441f9c2ac5d008b65
Mandana Vaziri <mvaziri@us.ibm.com>
2025-08-13 11:46:09 -0400

    cleanup
    Signed-off-by: Mandana Vaziri <mvaziri@us.ibm.com>

--- examples/tutorial/sdk/decorator.py
+++ examples/tutorial/sdk/decorator.py
@@ -1,18 +1,20 @@
 from pdl.pdl import pdl
 
+
 @pdl
 def my_pdl_program(scope):
     """
     model: ${ model }
     input: ${ input }
     """
-    return 
+    return
+
 
 @pdl
 def another(scope):
     """
     defs:
-        lib: 
+        lib:
             import: lib
     text:
     - ${ lib.hello }
@@ -20,7 +22,9 @@ def another(scope):
 
 
 def main():
-    result1 = my_pdl_program(scope={"model": "ollama_chat/granite3.2:2b", "input": "Hello\n"})
+    result1 = my_pdl_program(
+        scope={"model": "ollama_chat/granite3.2:2b", "input": "Hello\n"}
+    )
     result2 = another(scope={})
     print(result1)
     print(result2)
