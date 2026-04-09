851fba21ec1b71b65241732d861efda24bee2cb9
Louis Mandel <lmandel@us.ibm.com>
2026-04-08 23:03:11 -0400

    Fix dependency order
    Signed-off-by: Louis Mandel <lmandel@us.ibm.com>

--- examples/rag/rag.py
+++ examples/rag/rag.py
@@ -1,5 +1,5 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from litellm import embedding
 from litellm.exceptions import APIConnectionError
 from pymilvus import MilvusClient
